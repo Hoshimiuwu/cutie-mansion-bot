bfa9f9d0830d0c050078cebc2e01e38496ad12be
Jumpyuwu <szymonkaczmarek2002@gmail.com>
2021-02-27 11:50:22 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot,message, args) => {
     .setColor("#f5b0f0")
     .addField("Moderation:", "tempmute, clear.")
     .addField("Fun:", "crim, pog, uwu, xqc, 8ball, say, microwave.")
-    .addField("Interactive (kinda not working):", "hug, cuddle, lick")
+    .addField("Interactive (limited gifs for now):", "hug, cuddle, lick, poke")
     .addField("Not fully working yet:", "tempmute")
     .addField("Contact:", "If you have extra questions, feel free to contact me @Jumpy#0003")
     .setThumbnail('https://cdn.discordapp.com/attachments/624997004094996481/713038757246402650/JPEG_20200505_094831.jpg')
