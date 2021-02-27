8ad4f18a800e192f7f4e809aa60b0fe682a62936
Jumpyuwu <szymonkaczmarek2002@gmail.com>
2021-02-27 12:04:45 +0000

    Update microwave.js

--- commands/microwave.js
+++ commands/microwave.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+    return message.channel.send("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM BEEP BEEP BEEEEEEP!");
 }
 
 module.exports.help = {
