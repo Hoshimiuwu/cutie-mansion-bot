6f6eae26607d4cf4f7d06ff9513e46910d6c688f
Jumpyuwu <szymonkaczmarek2002@gmail.com>
2021-02-27 12:37:58 +0000

    Update Say.js

--- commands/Say.js
+++ commands/Say.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot,message, args) => {
     //says to whatever you say
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Innifcient perms");
     let botmessage = args.join(" ");
     message.delete().catch();
     message.channel.send(botmessage);
